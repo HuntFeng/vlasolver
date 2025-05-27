c7f1c9422cff8588f79d40dc9def2c6077c80072
HuntFeng <spacecurvature@gmail.com>
2025-05-27 06:06:24 +0000

    update

--- poisson.cpp
+++ poisson.cpp
@@ -1,100 +1,51 @@
-#include <sstream>
+#include <Kokkos_Core.hpp>
+#include <gridformat/gridformat.hpp>
+#include <impl/Kokkos_Profiling.hpp>
 
-#include "Kokkos_Core.hpp"
+// TODO: use SOR for the poisson solver
+// TODO: eventually we would like to use FFT method
+// people.eecs.berkeley.edu/~demmel/cs267/lecture24/lecture24.html#link_3
+int main(int argc, char* argv[]) {
+    Kokkos::ScopeGuard guard(argc, argv);
 
-#include "KokkosKernels_default_types.hpp"
-#include "KokkosSparse_CrsMatrix.hpp"
-#include "KokkosSparse_spmv.hpp"
+    const int nx = 128, ny = 128;
+    GridFormat::ImageGrid<2, double> grid{
+        {1.0, 1.0}, // domain size
+        {nx, ny}    // number of cells (pixels) in each direction
+    };
+    auto [dx, dy] = grid.spacing();
+    GridFormat::VTKHDFTimeSeriesWriter writer{grid, "data"};
 
-using Scalar = KokkosKernels::default_scalar;
-using Ordinal = KokkosKernels::default_lno_t;
-using Offset = KokkosKernels::default_size_type;
-using Layout = KokkosKernels::default_layout;
+    // potential field
+    Kokkos::View<double**> phi("phi", nx, ny);
+    Kokkos::View<double**> phi_new("phi_new", nx, ny);
+    Kokkos::View<double**> b("b", nx, ny);
 
-int main() {
-    Kokkos::ScopeGuard guard;
-    using device_type =
-        typename Kokkos::Device<Kokkos::DefaultExecutionSpace, typename Kokkos::DefaultExecutionSpace::memory_space>;
-    using matrix_type = typename KokkosSparse::CrsMatrix<Scalar, Ordinal, device_type, void, Offset>;
-    using graph_type = typename matrix_type::staticcrsgraph_type;
-    using row_map_type = typename graph_type::row_map_type;
-    using entries_type = typename graph_type::entries_type;
-    using values_type = typename matrix_type::values_type;
-
-    const Scalar SC_ONE = Kokkos::ArithTraits<Scalar>::one();
-
-    Ordinal numRows = 10;
-
-    {
-        const Offset numNNZ = 2 + (numRows - 2) * 3 + 2;
-        typename row_map_type::non_const_type row_map("row pointers", numRows + 1);
-        typename entries_type::non_const_type entries("column indices", numNNZ);
-        typename values_type::non_const_type values("values", numNNZ);
-
-        {
-            // Build the row pointers and store numNNZ
-            typename row_map_type::HostMirror row_map_h = Kokkos::create_mirror_view(row_map);
-            for (Ordinal rowIdx = 1; rowIdx < numRows + 1; ++rowIdx) {
-                if ((rowIdx == 1) || (rowIdx == numRows)) {
-                    row_map_h(rowIdx) = row_map_h(rowIdx - 1) + 2;
-                } else {
-                    row_map_h(rowIdx) = row_map_h(rowIdx - 1) + 3;
-                }
+    // Initialize phi
+    Kokkos::parallel_for(
+        Kokkos::MDRangePolicy({0, 0}, {nx, ny}), KOKKOS_LAMBDA(int i, int j) {
+            double x = i * dx; // y-coordinate
+            if (j == 0) {
+                phi(i, j) = x * (1.0 - x);
+            } else {
+                phi(i, j) = 0.0;
             }
-            Kokkos::deep_copy(row_map, row_map_h);
-            if (row_map_h(numRows) != numNNZ) {
-                std::ostringstream error_msg;
-                error_msg << "error: row_map(numRows) != numNNZ, row_map_h(numRows)=" << row_map_h(numRows)
-                          << ", numNNZ=" << numNNZ;
-                throw std::runtime_error(error_msg.str());
-            }
+        });
+    Kokkos::deep_copy(phi_new, phi);
+    Kokkos::deep_copy(b, 0.0);
 
-            typename entries_type::HostMirror entries_h = Kokkos::create_mirror_view(entries);
-            typename values_type::HostMirror values_h = Kokkos::create_mirror_view(values);
-            for (Ordinal rowIdx = 0; rowIdx < numRows; ++rowIdx) {
-                if (rowIdx == 0) {
-                    entries_h(row_map_h(rowIdx)) = rowIdx;
-                    entries_h(row_map_h(rowIdx) + 1) = rowIdx + 1;
+    writer.set_cell_field("phi", [&](const auto cell) { return phi_new(cell.location[0], cell.location[1]); });
+    writer.write(0);
 
-                    values_h(row_map_h(rowIdx)) = SC_ONE;
-                    values_h(row_map_h(rowIdx) + 1) = -SC_ONE;
-                } else if (rowIdx == numRows - 1) {
-                    entries_h(row_map_h(rowIdx)) = rowIdx - 1;
-                    entries_h(row_map_h(rowIdx) + 1) = rowIdx;
-
-                    values_h(row_map_h(rowIdx)) = -SC_ONE;
-                    values_h(row_map_h(rowIdx) + 1) = SC_ONE;
-                } else {
-                    entries_h(row_map_h(rowIdx)) = rowIdx - 1;
-                    entries_h(row_map_h(rowIdx) + 1) = rowIdx;
-                    entries_h(row_map_h(rowIdx) + 2) = rowIdx + 1;
-
-                    values_h(row_map_h(rowIdx)) = -SC_ONE;
-                    values_h(row_map_h(rowIdx) + 1) = SC_ONE + SC_ONE;
-                    values_h(row_map_h(rowIdx) + 2) = -SC_ONE;
-                }
-            }
-            // Copy the host views to device views
-            for (Ordinal rowIdx = 0; rowIdx < numRows + 1; ++rowIdx) {
-                Kokkos::printf("row_map_h(%d) = %d\n", rowIdx, row_map_h(rowIdx));
-            }
-            for (Ordinal i = 0; i < numNNZ; ++i) {
-                Kokkos::printf("entries_h(%d) = %d\n", i, entries_h(i));
-            }
-            for (Ordinal i = 0; i < numNNZ; ++i) {
-                Kokkos::printf("values_h(%d) = %f\n", i, values_h(i));
-            }
-            Kokkos::deep_copy(entries, entries_h);
-            Kokkos::deep_copy(values, values_h);
-        }
-
-        // graph_type myGraph(entries, row_map);
-        // matrix_type myMatrix("test matrix", numRows, values, myGraph);
-        matrix_type myMatrix("test matrix", numRows, numRows, numNNZ, values, row_map, entries);
-        std::cout << "myMatrix has been created successfully:" << std::endl
-                  << "  - numRows=" << myMatrix.numRows() << std::endl
-                  << "  - numCols=" << myMatrix.numCols() << std::endl
-                  << "  - numNNZ= " << myMatrix.nnz() << std::endl;
+    // Jacobi iteration
+    for (int iter = 1; iter < 100; ++iter) {
+        Kokkos::parallel_for(
+            Kokkos::MDRangePolicy({1, 1}, {nx - 1, ny - 1}), KOKKOS_LAMBDA(int i, int j) {
+                phi_new(i, j) = 0.25 * (phi(i - 1, j) + phi(i + 1, j) + phi(i, j - 1) + phi(i, j + 1) + b(i, j));
+            });
+        Kokkos::deep_copy(phi, phi_new);
+        writer.set_cell_field("phi", [&](const auto cell) { return phi_new(cell.location[0], cell.location[1]); });
+        writer.write(iter);
     }
 
     return 0;

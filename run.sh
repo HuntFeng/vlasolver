60e3eb11a32ecfd671abe58e32539a710e7e0c36
HuntFeng <spacecurvature@gmail.com>
2025-09-25 12:48:00 -0400

    update slurm script

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --account=CHEN_CUI
 #SBATCH --output=output_%j.log
-stdbuf -oL -eL apptainer run --nv .devcontainer/kokkos_cuda.sif ./build/sheath_full ./examples/sheath_full/input.ini
 
+# stdbuf -oL -eL apptainer run --nv .devcontainer/kokkos_cuda.sif ./build/sheath ./examples/sheath/input.ini
+stdbuf -oL -eL apptainer run --nv .devcontainer/kokkos_cuda.sif ./build/sheath_rough_wall ./examples/sheath_rough_wall/input.ini
+# stdbuf -oL -eL apptainer run --nv .devcontainer/kokkos_cuda.sif ./build/cylinder ./examples/cylinder/input.ini

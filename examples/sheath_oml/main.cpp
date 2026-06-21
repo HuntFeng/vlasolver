af28004a8a29a96cf904aa5d0e5725d3293cb1a1
HuntFeng <spacecurvature@gmail.com>
2026-06-21 14:51:25 -0700

    remove electrin injection control

--- examples/sheath_oml/main.cpp
+++ examples/sheath_oml/main.cpp
@@ -122,11 +122,9 @@ struct ImmersedWorld : World<ImmersedWorld> {
                     } else if (eta(i, j) < 0.0 && vx * n1 + vy * n2 > 0.0) {
                         f(i, j, iv, jv, 0) = 0.0; // immersed wall absorbs, emits nothing back into the plasma
                     } else if (j >= ny - ngc) {
-                        // top reservoir, with dynamic electron density adjustment
-                        double ne = (n(i, ny - ngc - 1, 0) > 0.0) ? n(i, ny - ngc - 1, 1) / n(i, ny - ngc - 1, 0) : 1.0;
                         f(i, j, iv, jv, 0) =
                             (vy <= v_ce) ? exp(-(pow(vx, 2) + pow(vy, 2)) / (2.0 * pow(v_th_e, 2)) + phi(i, j)) /
-                                               (2.0 * pi * pow(v_th_e, 2)) * ne
+                                               (2.0 * pi * pow(v_th_e, 2))
                                          : 0.0;
                     }
                 };

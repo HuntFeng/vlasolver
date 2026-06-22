26257574943117f69b6abceebe61a679bf4d9722
HuntFeng <spacecurvature@gmail.com>
2026-06-22 12:47:57 -0700

    must do surface charge calculation once per time step

--- examples/sheath_oml/main.cpp
+++ examples/sheath_oml/main.cpp
@@ -123,6 +123,9 @@ struct ImmersedWorld : World<ImmersedWorld> {
                     } else if (eta(i, j) < 0.0 && vx * n1 + vy * n2 > 0.0) {
                         f(i, j, iv, jv, 0) = 0.0; // immersed wall absorbs, emits nothing back into the plasma
                     } else if (j >= ny - ngc) {
+                        // FIXME: injection control for electron to eliminate the ion density bump near injection region
+                        // but this makes the simulation hard to stabilize because it changes the charge deposited on
+                        // the interface
                         double ne = (n(i, ny - ngc - 1, 0) > 0.0) ? n(i, ny - ngc - 1, 1) / n(i, ny - ngc - 1, 0) : 1.0;
                         f(i, j, iv, jv, 0) =
                             (vy <= v_ce) ? exp(-(pow(vx, 2) + pow(vy, 2)) / (2.0 * pow(v_th_e, 2)) + phi(i, j)) /
@@ -162,9 +165,9 @@ struct ImmersedWorld : World<ImmersedWorld> {
 
     void potential_boundary_conditions() {
         // only update when time advances, since the BCs are time-dependent through sigma_w
-        // if (current_step == last_step)
-        //     return;
-        // last_step                         = current_step;
+        if (current_step == last_step)
+            return;
+        last_step                         = current_step;
         auto& grid              = this->grid;
         auto [nx, ny, nvx, nvy] = grid.ncells;
         int ngc                 = grid.ngc;
